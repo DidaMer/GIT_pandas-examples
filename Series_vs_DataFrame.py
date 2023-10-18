d25263dd8fed704d1e8fcf97d21498a31275dd60
naloui1 <nadia.aloui@yahoo.fr>
2023-10-18 12:19:32 +0200

    ligne print supprime

--- Series_vs_DataFrame.py
+++ Series_vs_DataFrame.py
@@ -11,7 +11,7 @@ spanishPlayers = pd.Series(
     ['Casillas', 'Ramos', 'Pique', 'Puyol', 'Capdevila', 'Xabi Alonso', 'Busquets', 'Xavi Hernandez', 'Pedrito',
      'Iniesta', 'Villa'], index=[1, 15, 3, 5, 11, 14, 16, 8, 18, 6, 7])
 print("Spanish Football Players: \n%s" % spanishPlayers)
-print('\n==================================================================\n')
+
 
 
 # series through dictionary
